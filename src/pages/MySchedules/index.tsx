b2d739a34b577e71e475b8a0961708fec6bc204b
apsampaio <andre03.sampaio@gmail.com>
2020-10-18 15:49:39 -0300

    dayitem daylist, missing componentization

--- src/pages/MySchedules/index.tsx
+++ src/pages/MySchedules/index.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import { Link } from "react-router-dom";
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import { Link } from 'react-router-dom';
 
-import noScheduleImg from "../../assets/noSchedule.svg";
+import noScheduleImg from '../../assets/noSchedule.svg';
 
-import { useAuth, IAppointment } from "../../hooks/Auth";
+import { useAuth, IAppointment } from '../../hooks/Auth';
 
-import { Container, Content, List, EachDay, EachTime, EachDayItems } from "./styles";
+import { Container, Content, DayList, DayItem } from './styles';
 
 const MySchedules: React.FC = () => {
-  const [appointments, setAppointments] = useState<IAppointment[]>([])
-  const { user } = useAuth()
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
+  const { user } = useAuth();
 
   useEffect(() => {
-    setAppointments(user.appointments)
-  }, [user])
+    setAppointments(user.appointments);
+  }, [user]);
 
   if (!appointments) {
     return (
@@ -25,7 +25,7 @@ const MySchedules: React.FC = () => {
         <Content>
           <div>
             <strong>Meus Horários</strong>
-            <Link to='/schedule' >+ Novo horário</Link>
+            <Link to="/schedule">+ Novo horário</Link>
           </div>
           <img src={noScheduleImg} alt="Sem horários" />
           <h3>Ops! Parece que você ainda não agendou nenhum horario</h3>
@@ -42,39 +42,48 @@ const MySchedules: React.FC = () => {
       <Content>
         <div>
           <strong>Meus Horários</strong>
-          <Link to='/schedule' >+ Novo horário</Link>
+          <Link to="/schedule">+ Novo horário</Link>
         </div>
 
-        <List>
-          <EachDay>
-            <label>
-              Segunda-feira, 12
-            </label>
-
-            <EachDayItems>
-              <EachTime>
-                09:00
-              </EachTime>
-
-              <EachTime>
-                10:00
-              </EachTime>
-
-              <EachTime>
-                11:00
-              </EachTime>
-
-            </EachDayItems>
-          </EachDay>
-
-        </List>
-
+        <DayList>
+          <DayItem>
+            <label>Segunda-feira, 12</label>
+            <span>
+              <span>09:00</span>
+              <span>10:00</span>
+              <span>11:00</span>
+            </span>
+          </DayItem>
+          <DayItem>
+            <label>Terça-feira, 13</label>
+            <span>
+              <span>09:00</span>
+              <span>10:00</span>
+              <span>11:00</span>
+              <span>12:00</span>
+              <span>13:00</span>
+              <span>14:00</span>
+            </span>
+          </DayItem>
+          <DayItem>
+            <label>Quarta-feira, 14</label>
+            <span>
+              <span>09:00</span>
+              <span>10:00</span>
+              <span>11:00</span>
+              <span>12:00</span>
+              <span>13:00</span>
+              <span>14:00</span>
+              <span>15:00</span>
+              <span>16:00</span>
+              <span>17:00</span>
+            </span>
+          </DayItem>
+        </DayList>
       </Content>
       <Footer />
     </Container>
-  )
-
-
+  );
 };
 
 export default MySchedules;
--- src/pages/MySchedules/styles.ts
+++ src/pages/MySchedules/styles.ts
@@ -16,7 +16,7 @@ export const Content = styled.div`
   flex-grow: 1;
 
   margin-top: 64px;
-  margin-bottom: 64px;
+  margin-bottom: 32px;
 
   border-radius: 8px;
   border: 1px solid rgba(202, 83, 215, 0.5);
@@ -39,7 +39,6 @@ export const Content = styled.div`
 
     border-bottom: 1px solid rgba(202, 83, 215, 0.5);
     padding-bottom: 20px;
-    margin-bottom: 40px;
 
     strong {
       color: #32264d;
@@ -86,24 +85,62 @@ export const Content = styled.div`
   }
 `;
 
-export const List = styled.main`
+export const DayList = styled.ul`
+    display: flex;
+    flex-direction: column;
 `
 
-export const EachDay = styled.ul`
-  margin: 24px 64px;
+export const DayItem = styled.main`
+  width: 100%;
+  max-width: 736px;
+
+  flex-direction: column;
+
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  padding-left: 64px;
+  padding-right: 64px;
+  padding-top: 16px;
+
+  padding-bottom: 28px;
+  border-bottom: 1px solid rgba(202, 83, 215, 0.5);
+
+  &:last-of-type{
+    border-bottom: 0;
+  }
+
+  label {
+    color: #32264D;
+    font-weight: 500;
+  }
+
+  span {
+    flex-direction: row;
+    display: flex;
+
+    justify-content: flex-start;
+    flex-wrap: wrap;
+
+    padding-top: 12px;
+    padding-left: 15px;
+
+    span {
+      font-size: 16px;
+      color: #FFFCFE;
+      background: #4A2787;
+
+      padding: 15px 20px;
+      border: 1px solid #4A2787;
+      border-radius: 8px;
+
+      margin-right: 24px;
+      margin-top: 12px;
+    }
+
+  }
 
 
-`
 
-export const EachTime = styled.li`
-  list-style-type:none;
-  background-color:#4A2787;
-  padding: 16px;
-  border-radius:8px;
-  margin-right:24px;
-  color:#fff;
-`
 
-export const EachDayItems = styled.div`
-
-`
+`;
